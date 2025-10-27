def88cba82f4ec32c6204f58a199ccd882e5617d
zhangzhenfeng <zhenfeng.zhang@cyzone.cn>
2025-10-27 18:13:18 +0800

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -3,11 +3,11 @@ const isDev = process.env.NODE_ENV === 'development'
 export default {
   baseUrl: {
     dev: 'http://localhost:3002',
-    pro: 'http://api.dev.toimc.com:22000'
+    pro: 'http://zzf.net.cn:11005',
   },
   publicPath: [/^\/public/, /^\/login/],
   wsconfig: {
-    url: isDev ? '127.0.0.1' : 'api.dev.toimc.com',
-    port: isDev ? '3001' : 22001
-  }
+    url: isDev ? '127.0.0.1' : 'zzf.net.cn',
+    port: isDev ? '3001' : 22001,
+  },
 }
