318de8684484351a65ce1516abedf0ce72b39282
zhangzhenfeng <zhenfeng.zhang@cyzone.cn>
2025-10-27 14:36:56 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,14 @@ module.exports = {
   //   extends: ['plugin:vue/essential', '@vue/standard'],
   extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 0,
+    'no-debugger': 0,
+    'no-useless-escape': 0,
+    'no-mixed-spaces-and-tabs': 0,
+    'no-unused-vars': 'off',
+    quotes: ['error', 'double'],
   },
   parserOptions: {
     parser: 'babel-eslint',
