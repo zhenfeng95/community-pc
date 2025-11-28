3e33a8cf6bea8ef6d2fe709272b75a4670942a20
zhangzhenfeng <zhenfeng.zhang@cyzone.cn>
2025-11-28 14:58:36 +0800

    build

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,8 @@ export default {
   },
   publicPath: [/^\/v1\/public/, /^\/v1\/login/],
   wsconfig: {
-    url: isDev ? '127.0.0.1' : 'zzf.net.cn',
-    port: isDev ? '3001' : 22001,
+    url: isDev ? '127.0.0.1' : 'tech.zzf.net.cn/websocket',
+    port: isDev ? '3001' : 11007,
+    protocol: isDev ? 'ws' : 'wss',
   },
 }
